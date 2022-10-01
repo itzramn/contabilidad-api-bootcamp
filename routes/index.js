62b5d9ef23395dd445fa78392ba293e7ef1114e2
Itzel <itzelramonf@gmail.com>
2022-10-01 13:24:53 -0500

    CRUD Terminado

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { login } = require('../controllers/usuarios');
 const usuariosRouter = require('./usuarios');
+const categoriasRouter = require('./categorias');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hola Mundo');
 });
-router.post('/login', login);
 
+router.post('/login', login);
 router.use('/usuarios', usuariosRouter);
+router.use('/categorias', categoriasRouter);
+
 module.exports = router;
